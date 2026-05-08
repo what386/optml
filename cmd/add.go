94c2844d81dfc4e0578f23ee30db36eb27bdfa33
bmorin <robot0324@gmail.com>
2026-05-08 14:20:16 -0400

    impl add/remove

--- cmd/add.go
+++ cmd/add.go
@@ -1,14 +1,39 @@
 package cmd
 
-import "github.com/spf13/cobra"
+import (
+	"fmt"
+
+	"optml/internal"
+
+	"github.com/spf13/cobra"
+)
+
+var addOptRoot string
+var addMetadataPath string
 
 func init() {
+	addCmd.Flags().StringVar(&addOptRoot, "opt-root", "/opt", "installation root directory")
+	addCmd.Flags().StringVar(&addMetadataPath, "metadata-path", "/opt/metadata.json", "metadata JSON file path")
 	rootCmd.AddCommand(addCmd)
 }
 
 var addCmd = &cobra.Command{
-	Use:   "add <item>",
+	Use:   "add <name> <source>",
 	Short: "Add a program to /opt",
-	Args:  cobra.ExactArgs(1),
-	RunE:  runStub,
+	Args:  cobra.ExactArgs(2),
+	RunE: func(cmd *cobra.Command, args []string) error {
+		name := args[0]
+		source := args[1]
+
+		store := internal.NewMetadataStore(addMetadataPath)
+		manager := internal.NewOptManagerWithRoot(addOptRoot, store)
+
+		entry, err := manager.Add(name, source)
+		if err != nil {
+			return err
+		}
+
+		fmt.Fprintf(cmd.OutOrStdout(), "added %q to %s\n", entry.Name, entry.RootDir)
+		return nil
+	},
 }
--- cmd/remove.go
+++ cmd/remove.go
@@ -1,8 +1,19 @@
 package cmd
 
-import "github.com/spf13/cobra"
+import (
+	"fmt"
+
+	"optml/internal"
+
+	"github.com/spf13/cobra"
+)
+
+var removeOptRoot string
+var removeMetadataPath string
 
 func init() {
+	removeCmd.Flags().StringVar(&removeOptRoot, "opt-root", "/opt", "installation root directory")
+	removeCmd.Flags().StringVar(&removeMetadataPath, "metadata-path", "/opt/metadata.json", "metadata JSON file path")
 	rootCmd.AddCommand(removeCmd)
 }
 
@@ -10,5 +21,17 @@ var removeCmd = &cobra.Command{
 	Use:   "remove <item>",
 	Short: "Remove a program from /opt",
 	Args:  cobra.ExactArgs(1),
-	RunE:  runStub,
+	RunE: func(cmd *cobra.Command, args []string) error {
+		name := args[0]
+
+		store := internal.NewMetadataStore(removeMetadataPath)
+		manager := internal.NewOptManagerWithRoot(removeOptRoot, store)
+
+		if err := manager.Remove(name); err != nil {
+			return err
+		}
+
+		fmt.Fprintf(cmd.OutOrStdout(), "removed %q\n", name)
+		return nil
+	},
 }
