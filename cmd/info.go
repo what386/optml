91f5638df27b147daf867972447c3654ea6722e9
bmorin <robot0324@gmail.com>
2026-05-08 15:09:20 -0400

    add info/list

--- cmd/info.go
+++ cmd/info.go
@@ -1,8 +1,19 @@
 package cmd
 
-import "github.com/spf13/cobra"
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+
+	"optml/internal/storage"
+
+	"github.com/spf13/cobra"
+)
+
+var infoMetadataPath string
 
 func init() {
+	infoCmd.Flags().StringVar(&infoMetadataPath, "metadata-path", "/opt/optml/metadata.json", "metadata JSON file path")
 	rootCmd.AddCommand(infoCmd)
 }
 
@@ -10,5 +21,22 @@ var infoCmd = &cobra.Command{
 	Use:   "info <item>",
 	Short: "Show metadata for a program",
 	Args:  cobra.ExactArgs(1),
-	RunE:  runStub,
+	RunE: func(cmd *cobra.Command, args []string) error {
+		name := args[0]
+		store := storage.NewMetadataStore(infoMetadataPath)
+		entry, err := store.Get(name)
+		if err != nil {
+			if errors.Is(err, storage.ErrNotFound) {
+				return fmt.Errorf("program %q not found", name)
+			}
+			return err
+		}
+
+		out, err := json.MarshalIndent(entry, "", "  ")
+		if err != nil {
+			return fmt.Errorf("encode metadata: %w", err)
+		}
+		fmt.Fprintln(cmd.OutOrStdout(), string(out))
+		return nil
+	},
 }
--- cmd/list.go
+++ cmd/list.go
@@ -1,8 +1,17 @@
 package cmd
 
-import "github.com/spf13/cobra"
+import (
+	"fmt"
+
+	"optml/internal/storage"
+
+	"github.com/spf13/cobra"
+)
+
+var listMetadataPath string
 
 func init() {
+	listCmd.Flags().StringVar(&listMetadataPath, "metadata-path", "/opt/optml/metadata.json", "metadata JSON file path")
 	rootCmd.AddCommand(listCmd)
 }
 
@@ -10,5 +19,21 @@ var listCmd = &cobra.Command{
 	Use:   "list",
 	Short: "List installed programs",
 	Args:  cobra.NoArgs,
-	RunE:  runStub,
+	RunE: func(cmd *cobra.Command, args []string) error {
+		store := storage.NewMetadataStore(listMetadataPath)
+		entries, err := store.List()
+		if err != nil {
+			return err
+		}
+
+		if len(entries) == 0 {
+			fmt.Fprintln(cmd.OutOrStdout(), "no installed programs")
+			return nil
+		}
+
+		for _, entry := range entries {
+			fmt.Fprintf(cmd.OutOrStdout(), "%s\t%s\n", entry.Name, entry.RootDir)
+		}
+		return nil
+	},
 }
