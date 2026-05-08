132594c57c78efd781c61fda320b5e4a51307b12
bmorin <robot0324@gmail.com>
2026-05-08 15:53:17 -0400

    add root check

--- main.go
+++ main.go
@@ -1,12 +1,20 @@
 package main
 
 import (
+	"fmt"
 	"os"
 
 	"optml/cmd"
+	"optml/internal"
 )
 
 func main() {
+	args := os.Args[1:]
+	if internal.CommandNeedsRoot(args) && !internal.IsRunningAsRoot() {
+		fmt.Fprintln(os.Stderr, "this command requires root privileges; run with sudo")
+		os.Exit(1)
+	}
+
 	if err := cmd.Execute(); err != nil {
 		os.Exit(1)
 	}
